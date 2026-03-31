82e077213a8af158337c1fe486e76b5d2c92ac67
Eythan ATTOUMANI SALIM <eythanattoumanisalim@gmail.com>
2026-03-31 11:32:04 +0200

    modifie classe Telecommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+
 public class Telecommande {
-    private Lampe[] lampes;
+    private ArrayList<Lampe> lampes;
 
+    public Telecommande(){
+        this.lampes = new ArrayList<>(50);
+    }
+    public void ajouterLampe(Lampe l){
+        this.lampes.add(l);
 
+    }
+    public void activerLampe(int indiceLampe){
+        this.lampes.get(indiceLampe).allumer();
+    }
+    public void desactiverLampe(int indiceLampe){
+        this.lampes.get(indiceLampe).eteindre();
+    }
+    public void  activerTout(){
+        for(int i=0 ; i<this.lampes.size();i++){
+            this.lampes.get(i).allumer();
+        }
+
+    }
+    public Lampe getLampe(int indiceLampe){
+        return this.lampes.get(indiceLampe);
+    }
+    public String toString(){
+        String aff="";
+        for(int i=0 ; i<this.lampes.size();i++){
+            aff+= this.lampes.get(i).toString()+"\n";
+        }
+        return aff;
+
+    }
 }

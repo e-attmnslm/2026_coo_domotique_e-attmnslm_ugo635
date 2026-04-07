52a0abf0b320db5f1b5d7d5dbbcd5ed920540ad3
ugo635 <ugolechien58901@gmail.com>
2026-04-07 14:29:16 +0200

    Ajout adaptateur

--- src/AdapteurCheminee.java
+++ src/AdapteurCheminee.java
@@ -1,3 +1,19 @@
-public class AdapteurCheminee {
+public class AdapteurCheminee implements Appareil {
+    private Cheminee cheminee;
 
+    public AdapteurCheminee(Cheminee c) {
+        this.cheminee = c;
+    }
+
+    public void allumer() {
+        cheminee.changerIntensite(100);
+    }
+
+    public void eteindre() {
+        cheminee.changerIntensite(0);
+    }
+
+    public String toString() {
+        return cheminee.toString();
+    }
 }

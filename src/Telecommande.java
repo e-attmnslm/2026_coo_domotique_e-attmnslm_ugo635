415ec14e544cb1cd329a21197bf57646a8eaa328
Eythan ATTOUMANI SALIM <eythanattoumanisalim@gmail.com>
2026-04-07 09:29:53 +0200

    modification des methodes par rapport à l'interface Appareil

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -18,82 +18,51 @@ public class Telecommande {
 	 * la liste des lampes a controler on utilise l'interface pour masquer
 	 * l'implementation a ce niveau
 	 */
-	private List<Lampe> lampes;
+	private List<Appareil> apps;
 
 	/**
 	 * construit une telecommande sans lampe
 	 */
 	public Telecommande() {
-		this.lampes = new ArrayList<Lampe>();
+		this.apps = new ArrayList<Appareil>();
 	}
 
-	/**
-	 * on ajoute une lampe a la liste des lampes controlees
-	 * 
-	 * @param nouvelleLampe
-	 *            nouvelle lampe a controler
-	 */
-	public void ajouterLampe(Lampe nouvelleLampe) {
-		this.lampes.add(nouvelleLampe);
-	}
 
-	/**
-	 * permet d'activer une lampe a partir de la telecommande
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a activer dans la telecommande
-	 */
-	public void activerLampe(int indiceLampe) {
-		// TODO les etudiants devraient verifier qu'on ne deborde pas du tableau
-		// dans leurs tests
-		if (indiceLampe < this.lampes.size())
-			this.lampes.get(indiceLampe).allumer();
-	}
 
-	/**
-	 * permet de desactiver une lampe en utilisant la telecommande
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a eteindre
-	 */
-	public void desactiverLampe(int indiceLampe) {
-		if (indiceLampe < this.lampes.size())
-			this.lampes.get(indiceLampe).eteindre();
+	public void ajoutAppareil(Appareil a){
+		this.apps.add(a);
+	}
+	public void activerAppareil(int i){
+		this.apps.get(i).allumer();
+	}
+	public void desactiverAppareil(int i){
+		this.apps.get(i).eteindre();
 	}
 
 	/**
 	 * permet d'activer l'ensemble des lampes
 	 */
 	public void activerTout() {
-		for (int i = 0; i < this.lampes.size(); i++)
+		for (int i = 0; i < this.apps.size(); i++)
 			// reutiliser du code existant
-			this.activerLampe(i);
+			this.activerAppareil(i);
 	}
 
+	public List<Appareil> getAppareil(){
+		return this.apps;
+	}
 	/**
 	 * descriptif de la telecommande retourne une ligne par lampe suivi du
 	 * status de la lampe.
 	 */
 	public String toString() {
 		String descriptif = "";
-		for (int i = 0; i < this.lampes.size(); i++) {
-			descriptif += i + "-" + this.lampes.get(i) + "\n";
+		for (int i = 0; i < this.apps.size(); i++) {
+			descriptif += i + "-" + this.apps.get(i) + "\n";
 		}
 		return (descriptif);
 	}
 
-	/**
-	 * get qui retourne une lampe utile pour les tests
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a tester
-	 * @return la lampe a l'indice indiceLampe
-	 */
-	public Lampe getLampe(int indiceLampe) {
-		if (indiceLampe < this.lampes.size())
-			return (this.lampes.get(indiceLampe));
-		else
-			return(null);
-	}
+
 
 }
